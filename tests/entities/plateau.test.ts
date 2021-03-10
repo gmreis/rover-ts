dedcd6afbceef8bb320bda0a0586b79f67a68a2b
Geraldo Reis <geraldo.reis@chaordicsystems.com>
2021-03-09 23:45:58 -0300

    Update plateau test descriptions

--- tests/entities/plateau.test.ts
+++ tests/entities/plateau.test.ts
@@ -4,9 +4,9 @@ import { Plateau } from "../../src/entities/plateau";
 
 
 describe('Plateau', function() {
-    it('Create new Plateau', function() {
+    it('Should returns the same size used when it was created.', function() {
         const sizeOne = { X: 5, Y: 4 };
-        const sizeTwo = { X: 5, Y: 4 };
+        const sizeTwo = { X: 3, Y: 3 };
 
         const plateuOne = new Plateau(sizeOne);
         const plateuTwo = new Plateau(sizeTwo);
@@ -24,27 +24,27 @@ describe('Plateau', function() {
             plateau = new Plateau(size);
         });
 
-        it('It should return false when a position the coordinate is not within the plateau.', function() {
+        it('Should return false when a position the coordinate is not within the plateau.', function() {
             assert.strictEqual(plateau.validPosition({ X: 10, Y: 10 }), false);
         });
 
-        it('It should return false when the X coordinate is more than the X size of plateau.', function() {
+        it('Should return false when the X coordinate is more than the X size of plateau.', function() {
             assert.strictEqual(plateau.validPosition({ X: 10, Y: 3 }), false);
         });
 
-        it('It should return false when the Y coordinate is more than the Y size of plateau.', function() {
+        it('Should return false when the Y coordinate is more than the Y size of plateau.', function() {
             assert.strictEqual(plateau.validPosition({ X: 4, Y: 10 }), false);
         });
 
-        it('It should return false when the X coordinate is negative number.', function() {
+        it('Should return false when the X coordinate is negative number.', function() {
             assert.strictEqual(plateau.validPosition({ X: -4, Y: 2 }), false);
         });
 
-        it('It should return false when the Y coordinate is negative number.', function() {
+        it('Should return false when the Y coordinate is negative number.', function() {
             assert.strictEqual(plateau.validPosition({ X: 4, Y: -1 }), false);
         });
 
-        it('It should return true when a position the coordinate is within the plateau.', function() {
+        it('Should return true when a position the coordinate is within the plateau.', function() {
             assert.strictEqual(plateau.validPosition({ X: 3, Y: 0 }), true);
         });
     });
